ebc306e352bdb8bce58d9f7f14eb41fb61c58727
woesss <yurets.droid@gmail.com>
2021-06-11 13:11:34 +0300

    Update ACRA

--- app/src/main/java/ru/playsoftware/j2meloader/crashes/AppCenterSender.java
+++ app/src/main/java/ru/playsoftware/j2meloader/crashes/AppCenterSender.java
@@ -18,6 +18,9 @@ package ru.playsoftware.j2meloader.crashes;
 
 import android.content.Context;
 import android.os.Build;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -29,43 +32,31 @@ import org.acra.ReportField;
 import org.acra.data.CrashReportData;
 import org.acra.sender.ReportSender;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.security.KeyManagementException;
 import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.Map;
 
-import javax.net.ssl.HttpsURLConnection;
-
-import androidx.annotation.NonNull;
-
 public class AppCenterSender implements ReportSender {
-	private static final String TAG = AppCenterSender.class.getName();
-	private static String BASE_URL = "https://in.appcenter.ms/logs?Api-Version=1.0.0";
-	private static String FORM_KEY = "eccaa4ce-92e9-46b2-8f40-89296b70a931";
+	private static final String TAG = AppCenterSender.class.getSimpleName();
+	private static final String BASE_URL = "https://in.appcenter.ms/logs?Api-Version=1.0.0";
+	private static final String FORM_KEY = "eccaa4ce-92e9-46b2-8f40-89296b70a931";
 
 	@Override
 	public void send(@NonNull Context context, @NonNull final CrashReportData report) {
 		final String log = (String) report.get(AppCenterCollector.APPCENTER_LOG);
-		TLSSocketFactory sf = null;
-		// Force TLSv1.2 for Android 4.1-4.4
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
-				&& Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
-			try {
-				sf = new TLSSocketFactory();
-			} catch (KeyManagementException e) {
-				e.printStackTrace();
-			} catch (NoSuchAlgorithmException e) {
-				e.printStackTrace();
-			}
+		if (log == null || log.isEmpty()) {
+			return;
 		}
+		// Force TLSv1.2 for Android 4.1-4.4
+		boolean forceTls12 = Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
+				&& Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP;
 
-		HurlStack hurlStack = new HurlStack(null, sf);
+		HurlStack hurlStack = new HurlStack(null, forceTls12 ? new TLSSocketFactory() : null);
 		RequestQueue queue = Volley.newRequestQueue(context, hurlStack);
-		StringRequest postRequest = new StringRequest(Request.Method.POST, BASE_URL, null,
-				error -> android.util.Log.e(TAG, "Response error", error)
+		StringRequest postRequest = new StringRequest(Request.Method.POST, BASE_URL,
+				response -> Log.d(TAG, "send success: " + response),
+				error -> Log.e(TAG, "Response error", error)
 		) {
 			@Override
 			public Map<String, String> getHeaders() {
