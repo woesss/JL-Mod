0c05b6c4fede00db7e04198a35a6a615d2e90407
jobs_xie <jobs_xie@enable-ets.com>
2021-06-16 13:48:50 +0300

    support set fps at runtime
    (cherry picked from commit 82df65f70941ce0034fd75ec4588274f02dabf4f)

--- app/src/main/java/javax/microedition/shell/MicroLoader.java
+++ app/src/main/java/javax/microedition/shell/MicroLoader.java
@@ -174,6 +174,11 @@ public class MicroLoader {
 		return params.orientation;
 	}
 
+	void setLimitFps(int fps) {
+		if (fps == -1) Canvas.setLimitFps(params.fpsLimit);
+		else Canvas.setLimitFps(fps);
+	}
+
 	void applyConfiguration() {
 		try {
 			// Apply configuration to the launching MIDlet
