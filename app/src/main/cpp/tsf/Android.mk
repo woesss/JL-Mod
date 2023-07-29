a36a2abe52354e657e6b08a16e56b835e7e2a3e2
woesss <yurets.droid@gmail.com>
2023-07-29 20:14:49 +0300

    MMAPI: add TinySoundFont

---
+++ app/src/main/cpp/tsf/Android.mk
@@ -0,0 +1,29 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES = \
+	mmapi_jni.cpp \
+
+LOCAL_CFLAGS += -O2 \
+
+LOCAL_C_INCLUDES := $(LOCAL_PATH) \
+
+LOCAL_LDLIBS := -llog
+
+LOCAL_MODULE := tsf
+
+LOCAL_SHARED_LIBRARIES := oboe mmapi
+
+ifeq ($(TARGET_ARCH_ABI),armeabi-v7a)
+    LOCAL_ARM_NEON := false
+endif
+
+# Don't strip debug builds
+ifeq ($(NDK_DEBUG),1)
+    cmd-strip :=
+endif
+
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-module,prefab/oboe)
---
+++ app/src/main/cpp/tsf/mmapi_jni.cpp
@@ -0,0 +1,3 @@
+//
+// Created by woesss on 29.07.2023.
+//
