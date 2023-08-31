cebd2c3fbb13b2e31db674c59ae747abb781c178
woesss <yurets.droid@gmail.com>
2023-08-31 21:31:13 +0300

    Support secondary storages in filepicker on A11

--- app/src/main/java/ru/playsoftware/j2meloader/filepicker/FilteredFilePickerFragment.java
+++ app/src/main/java/ru/playsoftware/j2meloader/filepicker/FilteredFilePickerFragment.java
@@ -18,19 +18,25 @@ package ru.playsoftware.j2meloader.filepicker;
 
 import android.content.Context;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Environment;
+import android.os.storage.StorageManager;
+import android.os.storage.StorageVolume;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.loader.app.LoaderManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.nononsenseapps.filepicker.FilePickerFragment;
 import com.nononsenseapps.filepicker.LogicHandler;
 
 import java.io.File;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
@@ -41,15 +47,25 @@ import ru.playsoftware.j2meloader.util.StoragePermissionHelper;
 public class FilteredFilePickerFragment extends FilePickerFragment {
 	private static final List<String> extList = Arrays.asList(".jad", ".jar", ".kjx");
 	private static final Stack<File> history = new Stack<>();
+	private final List<File> roots = new ArrayList<>();
 	private File mRequestedPath;
 	private final StoragePermissionHelper storagePermissionHelper = new StoragePermissionHelper(this, this::onPermissionResult);
 
 	@Override
 	public void onAttach(Context context) {
 		super.onAttach(context);
-		if (history.empty()) {
-			history.push(Environment.getExternalStorageDirectory());
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+			StorageManager sm = ContextCompat.getSystemService(requireContext(), StorageManager.class);
+			if (sm != null) {
+				for (StorageVolume volume : sm.getStorageVolumes()) {
+					roots.add(volume.getDirectory());
+				}
+			}
 		}
+		if (history.empty()) {
+			history.push(roots.isEmpty() ? Environment.getExternalStorageDirectory() : getRoot());
+		}
+
 		requireActivity().getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
 			@Override
 			public void handleOnBackPressed() {
@@ -61,6 +77,15 @@ public class FilteredFilePickerFragment extends FilePickerFragment {
 				}
 			}
 		});
+	}
+
+	@NonNull
+	@Override
+	public File getParent(@NonNull File from) {
+		if (roots.contains(from)) {
+			return getRoot();
+		}
+		return super.getParent(from);
 	}
 
 	@NonNull
@@ -132,6 +157,52 @@ public class FilteredFilePickerFragment extends FilePickerFragment {
 	public void goToDir(@NonNull File file) {
 		history.add(mCurrentPath);
 		super.goToDir(file);
+	}
+
+	@Override
+	protected void refresh(@NonNull File nextPath) {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R && compareFiles(nextPath, getRoot()) == 0) {
+			StorageManager sm = ContextCompat.getSystemService(requireContext(), StorageManager.class);
+			if (sm != null) {
+				if (hasPermission(nextPath)) {
+					mCurrentPath = nextPath;
+					isLoading = true;
+					List<File> files = new ArrayList<>();
+					for (StorageVolume volume : sm.getStorageVolumes()) {
+						files.add(volume.getDirectory());
+					}
+					mFiles.replaceAll(files);
+					isLoading = false;
+					mCheckedItems.clear();
+					mCheckedVisibleViewHolders.clear();
+					mAdapter.setList(mFiles);
+					if (mCurrentDirView != null) {
+						mCurrentDirView.setText(getFullPath(mCurrentPath));
+					}
+					// Stop loading now to avoid a refresh clearing the user's selections
+					LoaderManager.getInstance(this).destroyLoader(0);
+				} else {
+					handlePermission(nextPath);
+				}
+			}
+			return;
+		}
+		super.refresh(nextPath);
+	}
+
+	@NonNull
+	@Override
+	public String getName(@NonNull File path) {
+		if (roots.contains(path) && Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+			StorageManager sm = ContextCompat.getSystemService(requireContext(), StorageManager.class);
+			if (sm != null) {
+				StorageVolume volume = sm.getStorageVolume(path);
+				if (volume != null) {
+					return volume.getDescription(requireContext());
+				}
+			}
+		}
+		return super.getName(path);
 	}
 
 	private boolean isBackTop() {
